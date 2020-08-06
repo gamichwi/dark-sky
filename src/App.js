bfa2ea256433e0199208429ef98086ae242e0e96
gamichwi <gamichwi@gmail.com>
2020-08-06 15:52:38 +1000

    can query the API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-   <h1>Hello, world.</h1>
-  );
-}
+  const lat = "-37.8169479";
+  const long = "145.1814905";
+
+  fetch(`https://dark-sky.p.rapidapi.com/${lat},${long}?lang=en&units=auto`, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "dark-sky.p.rapidapi.com",
+      "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+    }
+  })
+  .then(response => {
+    if(response.ok) {
+      return response.json()
+    } else console.log('Not Succesful')
+  })
+  .then(data => console.log(data))
+
+  return <h1>Hello, world.</h1>;
+};
 
 export default App;
